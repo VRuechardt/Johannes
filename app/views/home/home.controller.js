b1e0c9b478416729aa740ceda9b5a1cdd4e33a1c
VRuechardt <valentin@ruechardt.de>
2016-02-28 18:16:48 +0100

    some layouting

--- app/views/home/home.controller.js
+++ app/views/home/home.controller.js
@@ -1,8 +1,32 @@
 'use strict';
 
-module.exports = ['$scope', function($scope) {
+module.exports = ['$scope', '$rootScope', function($scope, $rootScope) {
 
-    console.log('controller running');
-    $('.parallax').parallax();
+    $rootScope.page = 'home';
+
+    $scope.scrollHandler = function(e) {
+
+        var scrollY = window.scrollY;
+        $('.fullpage-parallax').css({
+            top: (-scrollY / 3) + 'px'
+        });
+
+        if(scrollY > 0) {
+            $('nav').css({
+                background: 'rgba(0, 0, 0, 0.5)'
+            });
+        } else {
+            $('nav').css({
+                background: 'rgba(0, 0, 0, 0)'
+            });
+        }
+
+    };
+
+    window.addEventListener('scroll', $scope.scrollHandler);
+
+    $scope.$on('$locationChangeStart', function( event ) {
+        window.removeEventListener('scroll', $scope.scrollHandler);
+    });
 
 }];
