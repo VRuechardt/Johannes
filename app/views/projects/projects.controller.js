6dd5d6aa2f9fd49aefac54c4e3b1a7175dce1ef5
VRuechardt <valentin@ruechardt.de>
2016-03-06 22:42:15 +0100

    stuff

--- app/views/projects/projects.controller.js
+++ app/views/projects/projects.controller.js
@@ -5,12 +5,16 @@ module.exports = ['$scope', '$rootScope', '$timeout', function($scope, $rootScop
     $rootScope.page = 'projects';
     $("html, body").animate({ scrollTop: 0}, 600);
 
+    $scope.swiper;
+
     $timeout(function() {
+
         $('.card').each(function(i, o) {
             $(o).delay(i*250).fadeIn(400);
         });
 
         $('nav').toggleClass('dark', true);
+
     });
 
     $scope.$on('$locationChangeStart', function( event ) {
@@ -19,7 +23,19 @@ module.exports = ['$scope', '$rootScope', '$timeout', function($scope, $rootScop
 
     $scope.openProject = function(i) {
 
-        $('#imageModal').openModal();
+        $('#imageModal').openModal({
+            ready: function() {
+                $scope.swiper = new Swiper('.swiper-container', {
+                    pagination: '.swiper-pagination',
+                    slidesPerView: 1,
+                    paginationClickable: true,
+                    spaceBetween: 0,
+                    keyboardControl: true,
+                    nextButton: '.swiper-button-next',
+                    prevButton: '.swiper-button-prev',
+                });
+            }
+        });
 
     };
 
