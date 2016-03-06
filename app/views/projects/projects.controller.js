58f01801b49ca54bb95b44b88046c5f93f0d1962
VRuechardt <valentin@ruechardt.de>
2016-03-06 21:54:04 +0100

    s2wipüer

--- app/views/projects/projects.controller.js
+++ app/views/projects/projects.controller.js
@@ -1,8 +1,26 @@
 'use strict';
 
-module.exports = ['$scope', '$rootScope', function($scope, $rootScope) {
+module.exports = ['$scope', '$rootScope', '$timeout', function($scope, $rootScope, $timeout) {
 
     $rootScope.page = 'projects';
     $("html, body").animate({ scrollTop: 0}, 600);
+
+    $timeout(function() {
+        $('.card').each(function(i, o) {
+            $(o).delay(i*250).fadeIn(400);
+        });
+
+        $('nav').toggleClass('dark', true);
+    });
+
+    $scope.$on('$locationChangeStart', function( event ) {
+        $('nav').toggleClass('dark', false);
+    });
+
+    $scope.openProject = function(i) {
+
+        $('#imageModal').openModal();
+
+    };
 
 }];
