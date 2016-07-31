afaf161dd1e67b17ab9b7234d2a26664480b0444
Valentin Rüchardt <valentin@ruechardt.de>
2016-07-31 15:50:25 +0200

    stuff

--- app/views/me/me.controller.js
+++ app/views/me/me.controller.js
@@ -5,5 +5,10 @@ module.exports = ['$scope', '$rootScope', function($scope, $rootScope) {
     $rootScope.page = 'me';
     $("html, body").animate({ scrollTop: 0}, 600);
 
+    $('nav').toggleClass('filled', true);
+    $scope.$on('$locationChangeStart', function( event ) {
+        $('nav').toggleClass('filled', false);
+    });
+
 
 }];
--- app/views/projects/projects.controller.js
+++ app/views/projects/projects.controller.js
@@ -2,8 +2,42 @@
 
 module.exports = ['$scope', '$rootScope', '$timeout', function($scope, $rootScope, $timeout) {
 
+    $scope.projectList = [
+        {
+            slides: [
+                {
+                    src: 'img/Pfeffermühle_1.jpg'
+                },
+                {
+                    src: 'img/Pfeffermühle_2.jpg'
+                },
+                {
+                    src: 'img/Pfeffermühle_3.jpg'
+                },
+                {
+                    src: 'img/Pfeffermühle_4.jpg'
+                },
+                {
+                    src: 'img/Pfeffermühle_5.jpg'
+                },
+                {
+                    src: 'img/Pfeffermühle_6.jpg'
+                },
+                {
+                    src: 'img/Pfeffermühle_7.jpg'
+                }
+            ]
+        }
+    ];
+
+    $scope.project = $scope.projectList[0];
+
     $rootScope.page = 'projects';
     $("html, body").animate({ scrollTop: 0}, 600);
+    $('nav').toggleClass('filled', true);
+    $scope.$on('$locationChangeStart', function( event ) {
+        $('nav').toggleClass('filled', false);
+    });
 
     $scope.swiper;
 
@@ -13,15 +47,12 @@ module.exports = ['$scope', '$rootScope', '$timeout', function($scope, $rootScop
             $(o).delay(i*250).fadeIn(400);
         });
 
-        $('nav').toggleClass('dark', true);
-
     });
 
-    $scope.$on('$locationChangeStart', function( event ) {
-        $('nav').toggleClass('dark', false);
-    });
 
     $scope.openProject = function(i) {
+
+        $scope.project = $scope.projectList[i];
 
         $('#imageModal').openModal({
             ready: function() {
